3f55c1edaeb042c59566113a07929c1679d41fb5
Crowdbotics <team@crowdbotics.com>
2023-10-14 00:49:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-snow-43938",
+  url: "https://divine-snow-43938.botics.co",
+  api: "https://divine-snow-43938.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
